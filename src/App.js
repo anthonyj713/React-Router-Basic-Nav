b8ffb111cda18d129cff3574cf8c7e72cc01843e
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-27 16:47:18 -0700

    MVP met

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
-  </div>
+
+
+<Switch>
+
+<Route exact path="/">
+  <Home />
+</Route>
+   
+<Route exact path="/about">
+    <About />
+</Route>
+      
+<Route exact path="/contact">
+    <Contact />
+</Route>
+
+</Switch>
+
+</div>
 );
 
 export default App;
